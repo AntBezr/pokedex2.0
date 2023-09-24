1bfb69156922dcd67228ebfaab730853a8cf932d
AntBezr <sosleepyanton@gmail.com>
2023-09-24 23:45:01 +0300

    mobile version horizontal scrolling fix

--- src/components/UI/RadioBtn.jsx
+++ src/components/UI/RadioBtn.jsx
@@ -23,79 +23,80 @@ const RadioBtn = ({ stateGenerationChanger, selectedRadioBtn, updateSelectedBtn 
   return (
     <div className="radio">
       <h2>Choose generation: </h2>
-
-      <input
-        type="radio"
-        id="1"
-        name="generations"
-        value="1"
-        onChange={radioBtnHandler}
-        defaultChecked={selectedRadioBtn === 1} />
-      <label htmlFor='1'>1</label>
-      <input
-        type="radio"
-        id="2"
-        name="generations"
-        value="2"
-        onChange={radioBtnHandler}
-        defaultChecked={selectedRadioBtn === 2} />
-      <label htmlFor='2'>2</label>
-      <input
-        type="radio"
-        id="3"
-        name="generations"
-        value="3"
-        onChange={radioBtnHandler}
-        defaultChecked={selectedRadioBtn === 3} />
-      <label htmlFor='3'>3</label>
-      <input
-        type="radio"
-        id="4"
-        name="generations"
-        value="4"
-        onChange={radioBtnHandler}
-        defaultChecked={selectedRadioBtn === 4} />
-      <label htmlFor='4'>4</label>
-      <input
-        type="radio"
-        id="5"
-        name="generations"
-        value="5"
-        onChange={radioBtnHandler}
-        defaultChecked={selectedRadioBtn === 5} />
-      <label htmlFor='5'>5</label>
-      <input
-        type="radio"
-        id="6"
-        name="generations"
-        value="6"
-        onChange={radioBtnHandler}
-        defaultChecked={selectedRadioBtn === 6} />
-      <label htmlFor='6'>6</label>
-      <input
-        type="radio"
-        id="7"
-        name="generations"
-        value="7"
-        onChange={radioBtnHandler}
-        defaultChecked={selectedRadioBtn === 7} />
-      <label htmlFor='7'>7</label>
-      <input
-        type="radio"
-        id="8"
-        name="generations"
-        value="8"
-        onChange={radioBtnHandler}
-        defaultChecked={selectedRadioBtn === 8} />
-      <label htmlFor='8'>8</label>
-      <input
-        type="radio"
-        id="9"
-        name="generations"
-        value="9"
-        onChange={radioBtnHandler}
-        defaultChecked={selectedRadioBtn === 9} />
-      <label htmlFor='9'>9</label>
+      <div className="generations">
+        <input
+          type="radio"
+          id="1"
+          name="generations"
+          value="1"
+          onChange={radioBtnHandler}
+          defaultChecked={selectedRadioBtn === 1} />
+        <label htmlFor='1'>1</label>
+        <input
+          type="radio"
+          id="2"
+          name="generations"
+          value="2"
+          onChange={radioBtnHandler}
+          defaultChecked={selectedRadioBtn === 2} />
+        <label htmlFor='2'>2</label>
+        <input
+          type="radio"
+          id="3"
+          name="generations"
+          value="3"
+          onChange={radioBtnHandler}
+          defaultChecked={selectedRadioBtn === 3} />
+        <label htmlFor='3'>3</label>
+        <input
+          type="radio"
+          id="4"
+          name="generations"
+          value="4"
+          onChange={radioBtnHandler}
+          defaultChecked={selectedRadioBtn === 4} />
+        <label htmlFor='4'>4</label>
+        <input
+          type="radio"
+          id="5"
+          name="generations"
+          value="5"
+          onChange={radioBtnHandler}
+          defaultChecked={selectedRadioBtn === 5} />
+        <label htmlFor='5'>5</label>
+        <input
+          type="radio"
+          id="6"
+          name="generations"
+          value="6"
+          onChange={radioBtnHandler}
+          defaultChecked={selectedRadioBtn === 6} />
+        <label htmlFor='6'>6</label>
+        <input
+          type="radio"
+          id="7"
+          name="generations"
+          value="7"
+          onChange={radioBtnHandler}
+          defaultChecked={selectedRadioBtn === 7} />
+        <label htmlFor='7'>7</label>
+        <input
+          type="radio"
+          id="8"
+          name="generations"
+          value="8"
+          onChange={radioBtnHandler}
+          defaultChecked={selectedRadioBtn === 8} />
+        <label htmlFor='8'>8</label>
+        <input
+          type="radio"
+          id="9"
+          name="generations"
+          value="9"
+          onChange={radioBtnHandler}
+          defaultChecked={selectedRadioBtn === 9} />
+        <label htmlFor='9'>9</label>
+      </div>
     </div>
   );
 };
